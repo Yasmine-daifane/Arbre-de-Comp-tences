73748bc42e314aebd47445f14cf21934c555cff1
yasmine <110662044+Yasmine-daifane@users.noreply.github.com>
2023-10-03 08:39:27 -0700

    push application

---
+++ php-3tiers2/Application/Tests copy/StudentBLOTest.php
@@ -0,0 +1,9 @@
+<?php 
+
+require_once '../../loader.php' ;
+
+$testFunctions = new  StudentBLO() ;
+$printData = $testFunctions -> GetStudent (1) ;
+print_r($printData) ;
+
+?>
---
+++ php-3tiers2/Application/Tests copy/StudentDAOTest.php
@@ -0,0 +1,8 @@
+<?php 
+
+require_once '../../loader.php ';
+
+$testFunctions = new studentDAO() ; 
+ print_r ($printData); 
+
+?>
