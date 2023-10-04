005271babf9a89c7a4efdd934a2df323535e9d5f
yasmine <110662044+Yasmine-daifane@users.noreply.github.com>
2023-10-04 04:07:57 -0700

    add

--- php-3tiers2/add.php
+++ php-3tiers2/add.php
@@ -7,32 +7,36 @@ if(isset ($_POST ['studentSubmitButton'])&& $_POST ['studentSubmitButton'] == 'A
 
     $studentBllObj =new studentBLO(); 
     $studentName = $_POST ['studentName'];
-    $student
+    $studentRoll = $_POST['studentRoll'] ;
+    $studentEmail = $_POST ['studentEmail'] ;
+    $studentDateOfBirth = $_POST ['studentDateOfBirth'] ;
+
+    $newStudent = new Student (0,$studentName , $studentEmail ,$studentDateOfBirth) ;
+    $addStudentResult = $studentBllObj ->AddStudent ($newStudent) ;
+
+    if ($addStudentResult >0) {
+       header ("Location :edit.php?id=" . $addStudentResult . '&action=add');
+    }else {
+         
+        if($studentBllObj -> errorMessage !='') {
+            $errorMessage = $studentBllObj ->errorMessage ;
+        } else {
+
+            $errorMessage = 'Record can\'t be added . Operation failed .' ;
+        }
+    }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$pageTitle = 'Add New Student ' ;
+include_once ("Templates/header.php") ;
 
 ?>
+
+<div class="page-header" >
+    <h1> Add New Student  </h1> 
+
+</div>
+
+<?php  if($errorMessage !='') :    ?>
+
+    <div class=
